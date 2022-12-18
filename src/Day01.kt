495578e0ea750b093434547cad083d0194081bd9
Gnu Phobia <gnuphobia@gmail.com>
2022-12-18 09:32:23 -0500

    Solved day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,50 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var largest = 0
+        var total = 0
+
+        for (row in input) {
+            if (row.isBlank()) {
+                if (total > largest) {
+                    largest = total
+                }
+
+                total = 0
+            } else {
+                total += Integer.parseInt(row)
+            }
+        }
+
+        return largest
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val calories = mutableListOf<Int>()
+
+        var total = 0
+        for (row in input) {
+            if (row.isBlank() || row.isEmpty()) {
+                calories.add(total)
+                total = 0
+                continue
+            }
+
+            total += Integer.parseInt(row);
+        }
+        calories.add(total)
+
+        calories.sortDescending()
+
+        return calories[0] + calories[1] + calories[2]
     }
 
     // test if implementation meets criteria from the description, like:
+    var part1TestResult = 24000
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == part1TestResult)
+
+    var part2TestResult = 45000
+    check(part2(testInput) == part2TestResult)
 
     val input = readInput("Day01")
     part1(input).println()
